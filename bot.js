026fcb99b6b3591bbcd07b632c773325d3e957f9
Jismiodjaijionaodinaoin <41239192+YouSick@users.noreply.github.com>
2019-01-06 13:25:34 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,25 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("480165666142420992")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`Spam By ThatMyBoy`);
 }, 30)
 })
+
+
+client.on('message', message => {
+  var prefix = ".";
+  
+if (message.author.bot) return;
+if (!message.content.startsWith(prefix)) return;
+   
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+   
+let args = message.content.split(" ").slice(1);
+   
+if (command === "say") {
+message.delete()
+  message.channel.sendMessage(args.join(" ")).catch(console.error);
+}
 
 client.login(process.env.BOT_TOKEN);
