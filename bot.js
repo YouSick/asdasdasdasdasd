0d67ab5ef2eef44b098bde8087dbbf7d49a94f1e
Jismiodjaijionaodinaoin <41239192+YouSick@users.noreply.github.com>
2019-01-06 13:59:12 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("480165666142420992")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`ThatMyBoy`);
 }, 30)
 })
 
